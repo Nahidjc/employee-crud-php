1f3c0da99d999eb6ec4f45bdce801b9717b279fe
Nahidjc <nahidjc80@gmail.com>
2021-08-09 09:43:18 +0600

    employee details updated

--- index.php
+++ index.php
@@ -52,29 +52,6 @@
     $result = mysqli_query($mysqli, "SELECT * FROM employee");
     ?>
 
-    <!-- Modal form -->
-    <!-- <div class="modal fade" id="formModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Add Employee</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form action="">
-                        <input type="text" placeholder="Employee name">
-                        <input type="text" placeholder="Employee Addres">
-                        <input type="text" placeholder="salary">
-                        <input type="submit" class="bg-primary m-3 rounded text-white">
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save</button>
-                </div>
-            </div>
-        </div>
-    </div> -->
 
     <section style="height:100%">
         <main class="p-5 container border border-lg shadow my-5">
